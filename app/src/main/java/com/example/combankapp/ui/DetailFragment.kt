35dba88761793b60c10aa3b0c7403b33328c7819
uday singh <>
2024-06-23 00:04:25 +1000

    Added Navhost

---
+++ app/src/main/java/com/example/combankapp/ui/DetailFragment.kt
@@ -0,0 +1,43 @@
+package com.example.combankapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.combankapp.databinding.FragmentDetailBinding
+import com.example.combankapp.models.Transaction
+import javax.annotation.Nullable
+
+
+class DetailFragment : Fragment() {
+    private var binding: FragmentDetailBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding?.getRoot()
+    }
+
+    override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (arguments != null) {
+            val transaction: Transaction? =
+                requireArguments().getSerializable("transaction") as Transaction?
+            if (transaction != null) {
+                binding?.tvDescription?.text = transaction.description
+                binding?.tvAmount?.text = transaction.amount
+            }
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
--- app/src/main/java/com/example/combankapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/combankapp/ui/HomeFragment.kt
@@ -1,42 +1,42 @@
-package com.example.combankapp
+package com.example.combankapp.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.combankapp.databinding.ActivityMainBinding
+import com.example.combankapp.databinding.FragmentHomeBinding
 import com.example.combankapp.models.Transaction
 import com.example.combankapp.models.TransactionData
-import com.example.combankapp.ui.TransactionsAdapter
 import com.example.combankapp.viewmodel.TransactionViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import java.util.Collections
 import java.util.TreeMap
 
-
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class HomeFragment : Fragment() {
 
     private val viewModel: TransactionViewModel by viewModels()
 
-    private var binding: ActivityMainBinding? = null
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater);
-        setContentView(binding?.getRoot());
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         lifecycleScope.launch {
             viewModel.transactionsData.collect { data ->
                 data?.let {
@@ -47,13 +47,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     private fun processTransactionsData(transactionData: TransactionData) {
         val transactions: List<Transaction> = transactionData.transactions
 
         // Group transactions by date, Using a TreeMap ensures that the keys (dates) are sorted in natural order, which can be useful if you need the transactions to be displayed in a sorted manner
         val groupedTransactions: MutableMap<String, MutableList<Transaction>> =
             TreeMap<String, MutableList<Transaction>>(
-                Collections.reverseOrder<String>()
+                Collections.reverseOrder()
             )
         for (transaction in transactions) {
             val date: String = transaction.effectiveDate
@@ -62,8 +63,13 @@ class MainActivity : AppCompatActivity() {
             }
             groupedTransactions[date]?.add(transaction)
         }
-        binding?.recyclerView?.setLayoutManager(LinearLayoutManager(this));
-        binding?.recyclerView?.setAdapter(TransactionsAdapter(groupedTransactions));
+        binding.recyclerView.setLayoutManager(LinearLayoutManager(requireContext()))
+        binding.recyclerView.setAdapter(TransactionsAdapter(groupedTransactions))
         println("groupedTransactions$groupedTransactions")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
